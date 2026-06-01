19146984bc9cd8ee430b9b9f6c97518a25a51b78
ayodhyadharsha-max <ayodhyadharsha-max@users.noreply.github.com>
2026-06-01 21:32:47 +0530

    Replace header phone number text with clean WhatsApp label

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -56,8 +56,7 @@ export default function Navigation() {
                   <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
                 </svg>
               </div>
-              <span className="hidden md:inline">WhatsApp Inquiry: </span>
-              <span className="text-slate-100">+91 75720 94201</span>
+              <span className="text-slate-100">WhatsApp</span>
             </a>
 
             {/* CTA */}
